ec12e33cec4cc2a0aa7c2dc51e6c460f251824e6
Jaychou <2908912319@qq.com>
2024-04-08 10:32:20 +0800

    the first generation

---
+++ src/main/java/org/example/user_center_01/UserCenter01Application.java
@@ -0,0 +1,15 @@
+package org.example.user_center_01;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication                  // 复合注解
+@MapperScan("org.example.user_center_01.mapper")            // 加注解SpringBoot就能扫描Mapper下的文件夹
+public class UserCenter01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenter01Application.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/user_center_01/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package org.example.user_center_01.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.user_center_01.model.User;
+
+public interface UserMapper extends BaseMapper<User> {      // 在 UserMapper 类中实现操作数据库的方法（例如CURD）
+
+}
